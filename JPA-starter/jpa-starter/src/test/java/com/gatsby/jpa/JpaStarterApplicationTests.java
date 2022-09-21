fee506b29c64366772808dc5aba5bd03e31a640c
bgmer7 <caijinyang98@gmail.com>
2022-09-21 17:17:01 +0800

    jpa initial

---
+++ JPA-starter/jpa-starter/src/test/java/com/gatsby/jpa/JpaStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gatsby.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
